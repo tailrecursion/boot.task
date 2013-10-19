779eff26c477709196123d08add95d0989126840
Micha Niskin <asdf@ubergibson.com>
2013-10-19 10:36:41 -0400

    remove cljs dependency

--- README.md
+++ README.md
@@ -2,12 +2,30 @@
 
 A Clojure library designed to ... well, that part is up to you.
 
-## Usage
+### Example
 
 FIXME
+
+### Dependency
+
+Artifacts are published on [Clojars][1].
+
+```clojure
+[tailrecursion/boot.task "0.1.1"]
+```
+
+```xml
+<dependency>
+  <groupId>tailrecursion</groupId>
+  <artifactId>boot.task</artifactId>
+  <version>0.1.1</version>
+</dependency>
+```
 
 ## License
 
 Copyright © 2013 FIXME
 
 Distributed under the Eclipse Public License, the same as Clojure.
+
+[1]: https://clojars.org/tailrecursion/boot.task
--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject tailrecursion/boot.task "0.1.0"
+(defproject tailrecursion/boot.task "0.1.1"
   :description "Useful tasks for the boot Clojure build tool."
   :url "http://github.com/tailrecursion/boot.task"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.apache.maven/maven-model "3.0.4"
                   :exclusions [org.codehaus.plexus/plexus-utils]]
-                 [org.clojure/clojurescript "0.0-1934"]
                  [reply "0.2.0"]])
