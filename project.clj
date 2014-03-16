14f7b9c4dd77dc5ae7a68bc6d54ebcf93ee62aae
Micha Niskin <micha.niskin@gmail.com>
2014-03-16 10:55:00 -0400

    Remove pomegranate dep and update boot.core dep

--- project.clj
+++ project.clj
@@ -1,11 +1,9 @@
-(defproject tailrecursion/boot.task "2.1.1"
+(defproject tailrecursion/boot.task "2.1.2-SNAPSHOT"
   :description  "Useful tasks for the boot Clojure build tool."
   :url          "http://github.com/tailrecursion/boot.task"
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.apache.maven/maven-model "3.0.4"
-                  :exclusions [org.codehaus.plexus/plexus-utils]]
+  :dependencies [[org.apache.maven/maven-model "3.0.4"]
                  [tailrecursion/warp "0.1.0" :exclusions [org.clojure/clojure]]
-                 [tailrecursion/boot.core "2.2.1"]]
-  :profiles {:dev {:dependencies [[tailrecursion/boot "1.0.1"]
-                                  [tailrecursion/boot.core "2.0.0"]]}})
+                 [tailrecursion/boot.core "2.2.2-SNAPSHOT"]]
+  :profiles {:dev {:dependencies [[tailrecursion/boot "1.0.3-SNAPSHOT"]]}})
