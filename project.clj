041e12ec652c41248e36e56ac4f933362fd7869c
Micha Niskin <micha.niskin@gmail.com>
2014-04-01 16:25:12 -0400

    2.1.2 Remove plexus exclusion
    Boot 1.0.3 eliminated the pomegranate runtime dependency, which was the
    reason for the exclusion. Without the plexus dependency you'll get the
    org.codehaus.plexus.util.xml.pull.XmlSerializer exception.

--- project.clj
+++ project.clj
@@ -1,9 +1,8 @@
-(defproject tailrecursion/boot.task "2.1.2-SNAPSHOT"
+(defproject tailrecursion/boot.task "2.1.2"
   :description  "Useful tasks for the boot Clojure build tool."
   :url          "http://github.com/tailrecursion/boot.task"
   :license      {:name "Eclipse Public License"
                  :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.apache.maven/maven-model "3.0.4"]
-                 [tailrecursion/warp "0.1.0" :exclusions [org.clojure/clojure]]
-                 [tailrecursion/boot.core "2.2.2-SNAPSHOT"]]
+                 [tailrecursion/warp "0.1.0" :exclusions [org.clojure/clojure]]]
   :profiles {:dev {:dependencies [[tailrecursion/boot "1.0.3-SNAPSHOT"]]}})
